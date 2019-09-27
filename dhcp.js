40b09d3dbb9cac523b690473dfcea806ae15f553
Sava Heron <12586876+SavaHeron@users.noreply.github.com>
2019-09-27 18:15:50 +0100

    create dhcp.js

---
+++ dhcp.js
@@ -0,0 +1,10 @@
+/*
+Name: 		index.js
+Purpose:	A-Level Computer Science coursework DHCP script
+Author:		Sava Max Heron
+Created:	27/09/2019
+Copyright:	(c) SMH 2019
+Licence:	CC BY-NC-ND 4.0
+*/
+
+[placeholder]
